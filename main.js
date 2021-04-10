66d5170bba49134a2f39b3b77425ada074af6d20
Mykyta Nekh <nikita.nekh@gmail.com>
2021-04-11 02:26:59 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,40 @@
 const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
 ];
 const refs = {
   butStart: document.querySelector('[data-action="start"]'),
   butStop: document.querySelector('[data-action="stop"]'),
-    body: document.querySelector('body')
-}
+  body: document.querySelector("body"),
+};
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 let intervalId;
 function interval() {
-   intervalId = setInterval(() => refs.body.setAttribute('style', `background-color:${colors[randomIntegerFromInterval(0,colors.length)]}`), 1000)
+  intervalId = setInterval(
+    () =>
+      refs.body.setAttribute(
+        "style",
+        `background-color:${
+          colors[randomIntegerFromInterval(0, colors.length)]
+        }`
+      ),
+    1000
+  );
 }
-refs.butStop.setAttribute('disabled', 'true');
-refs.butStart.addEventListener('click', () => {
+refs.butStop.setAttribute("disabled", "true");
+refs.butStart.addEventListener("click", () => {
   interval();
-  refs.butStart.setAttribute('disabled', 'true');
-  refs.butStop.removeAttribute('disabled');
-
+  refs.butStart.setAttribute("disabled", "true");
+  refs.butStop.removeAttribute("disabled");
 });
-refs.butStop.addEventListener('click', () => {
+refs.butStop.addEventListener("click", () => {
   clearInterval(intervalId);
-  refs.butStart.removeAttribute('disabled');
-  refs.butStop.setAttribute('disabled', 'true');
-
-})
-
-
+  refs.butStart.removeAttribute("disabled");
+  refs.butStop.setAttribute("disabled", "true");
+});
